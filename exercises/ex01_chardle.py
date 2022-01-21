86cd54b14ad4a15b58759373c53d0de05d0d7bf2
Ariayana Harrell <ariayana@email.unc.edu>
2022-01-20 19:26:44 -0500

    ex01_updated_progress

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,19 +5,31 @@ if len(guess_word) != 5:
     print("Error: Word must contain 5 characters")
     exit()
 letter: str = input("Enter a single character: ")
-print("Searching for " + letter "in " + guess_word)
+print("Searching for " + letter + " in " + guess_word)
 if len(letter) != 1:
     print("Error: Character must be a single character.")
     exit()
+instance: int = 0
+
 if guess_word[0] == letter:
     print(letter + " found at index 0")
+    instance = instance + 1
 if guess_word[1] == letter:
     print(letter + " found at index 1")
+    instance = instance + 1
 if guess_word[2] == letter:
     print(letter + " found at index 2")
+    instance = instance + 1
 if guess_word[3] == letter:
     print(letter + " found at index 3")
+    instance = instance + 1
 if guess_word[4] == letter:
     print(letter + " found at index 4")
-instance: int = int(letter == letter) 
-print((instance) + " of" letter "is found in" + guess_word)
+    instance = instance + 1
+
+if instance == 1:
+    print("1 instance " + letter + " found in " + guess_word)
+if instance == 2:
+    print("2 instances of " + letter + " found in " + guess_word)
+if instance == 0:
+    print("No instances of " + letter + " found in " + guess_word)
